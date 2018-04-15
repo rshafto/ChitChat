00f9a98783c75abf9315b20beacf2f76c276d46d
Shafto, Robin <robin.shafto@mymail.champlain.edu>
2018-04-15 19:19:47 -0400

    table cell

---
+++ ChitChat/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  Alamofire iOS
+//
+//  Created by Shafto, Robin on 4/15/18.
+//  Copyright © 2018 Alamofire. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
