3d6aebffc0d2afc844fc2db907e10d5bdebd0cc6
Shafto, Robin <robin.shafto@mymail.champlain.edu>
2018-04-15 19:32:25 -0400

    Getting messages

--- ChitChat/MessageTableViewController.swift
+++ ChitChat/MessageTableViewController.swift
@@ -4,14 +4,29 @@
 //
 //  Created by Shafto, Robin on 4/15/18.
 //  Copyright © 2018 Alamofire. All rights reserved.
-//
+// key:735e6cff-5205-49b7-be80-7ec57c083aac
 
 import UIKit
+import Alamofire
 
 class MessageTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Alamofire.request("https://www.stepoutnyc.com/chitchat", method: .get, parameters: ["key" : "735e6cff-5205-49b7-be80-7ec57c083aac", "client" : "robin.shafto@mymail.champlain.edu"]).responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
